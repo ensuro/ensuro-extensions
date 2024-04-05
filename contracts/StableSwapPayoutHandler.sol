4fc52f6edda96f34e9c1d7e7cf6aac77dbe08902
GnP <g.parrondo@gmail.com>
2024-04-05 09:16:40 -0300

    Fix gap

--- contracts/StableSwapPayoutHandler.sol
+++ contracts/StableSwapPayoutHandler.sol
@@ -300,5 +300,5 @@ contract StableSwapPayoutHandler is
    * variables without shifting down storage in the inheritance chain.
    * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
    */
-  uint256[45] private __gap;
+  uint256[44] private __gap;
 }
